52c9201a0b9f67d9b6a9e6e3f1213f11b5583cfe
LeonardoDeFaveri <leonardo.defaveri01@gmail.com>
2022-11-10 11:18:53 +0100

    Fixed duration_str method

--- src/query.rs
+++ src/query.rs
@@ -1,4 +1,4 @@
-use std::{error::Error, time::Duration};
+use std::{error::Error, time::Duration, vec};
 
 /// Holds the information required
 /// to make a query.
@@ -106,6 +106,12 @@ impl QueryResult {
     }
 }
 
+impl Default for QueryResult {
+    fn default() -> Self {
+        Self { data: vec![] }
+    }
+}
+
 /// Each plugin has to implement this trait to define how a piece
 /// of information pulled from the plugin's service can be
 /// deserialized and transformed into the selected result type.
@@ -181,14 +187,19 @@ impl QueryResultData {
         &self.duration
     }
 
-    pub fn duration_str(duration: std::time::Duration) -> String {
-        let secs = duration.as_secs();
+    pub fn duration_str(&self) -> String {
+        let secs = self.duration.as_secs();
         let mut mins: u64 = secs / 60;
         if mins > 60 {
             let hours: u64 = mins / 60;
             mins = mins - hours * 60;
-            return format!("{}:{:02}:{:02}", hours, mins, secs - hours * 3600 - mins * 60);
+            return format!(
+                "{}:{:02}:{:02}",
+                hours,
+                mins,
+                secs - hours * 3600 - mins * 60
+            );
         }
-        return format!("{}:{}", mins, secs - mins * 60);
+        return format!("{:02}:{:02}", mins, secs - mins * 60);
     }
 }
